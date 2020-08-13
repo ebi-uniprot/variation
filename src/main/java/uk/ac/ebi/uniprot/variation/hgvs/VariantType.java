7c60ddb46472b661b54803b2a3c4d09bc2cad168
anight <anight@ebi.ac.uk>
2020-08-13 16:52:55 +0100

    Added functionality to VariantType to provide consequenceTypes correctly.

--- src/main/java/uk/ac/ebi/uniprot/variation/hgvs/VariantType.java
+++ src/main/java/uk/ac/ebi/uniprot/variation/hgvs/VariantType.java
@@ -1,16 +1,40 @@
 package uk.ac.ebi.uniprot.variation.hgvs;
 
 public enum VariantType {
-	SUBSTITUTION,
-    DELETION,
-    DUPLICATION,
-    INSERTION,
-    DELETION_INSERTION,
-    FRAMESHIFT,
-    REPEAT,
-    EXTENSION,
-    INVERSION,
-    INSERTION_INVERSION,
-    CONVERSION,
-    UNKNOWN;
+	SUBSTITUTION("substitution"),
+    DELETION("deletion"),
+    DUPLICATION("duplication"),
+    INSERTION("insertion"),
+    DELETION_INSERTION("deletion insertion"),
+    FRAMESHIFT("frameshift"),
+    REPEAT("repeat"),
+    EXTENSION("extension"),
+    INVERSION("inversion"),
+    INSERTION_INVERSION("insertion inversion"),
+    CONVERSION("conversion"),
+    MISSENSE("missense"),
+    NONSENSE("nonsense"),
+    STOPLOST("stop lost"),
+    STOPGAINED("stop gained"),
+    SILENT("silent"),
+    UNKNOWN("unknown");
+	
+	private final String consequenceName;
+	
+	VariantType(String name) {
+		this.consequenceName = name;
+	}
+	
+	public String getConsequenceType() {
+		return consequenceName;
+	}
+	
+	public static VariantType getVariantType(String variantType) {
+		for (VariantType v : VariantType.values()) {
+			if(variantType.equalsIgnoreCase(v.getConsequenceType()))
+			  return v;
+		}
+		return UNKNOWN;
+	}
+	
 }
